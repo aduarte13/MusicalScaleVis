faccd2f4cf937d6b16a3a30aef4fd80f9d466807
aduarte13 <aduarte496@yahoo.com>
2023-08-24 11:55:37 -0500

    added diatonic chords to Scale.kt represented in Nashville number system for the 7 major modes

--- src/main/kotlin/Scale.kt
+++ src/main/kotlin/Scale.kt
@@ -1,29 +1,36 @@
+import org.w3c.dom.stylesheets.LinkStyle
+
 class Scale(
-    private var rootNote: String,
-    private var modeType: String,
-    private var flatsOrSharps: Int = 1,
-    private val diatonicNotes: MutableList<String> = mutableListOf<String>(),
-    private val cleanNotes: MutableList<String> = mutableListOf<String>(),
-    private var formulaStrings: MutableList<String> = mutableListOf<String>(),
-    private var formulaInts: MutableList<Int> = mutableListOf<Int>(),
-    private val intervalStrings: Map<Int, String> = mapOf(
+        private var rootNote: String,
+        private var modeType: String,
+        private var flatsOrSharps: Int = 1,
+        private val diatonicNotes: MutableList<String> = mutableListOf<String>(),
+        private val cleanNotes: MutableList<String> = mutableListOf<String>(),
+        private var diatonicChords: MutableList<String> = mutableListOf<String>(),
+        private var formulaStrings: MutableList<String> = mutableListOf<String>(),
+        private var formulaInts: MutableList<Int> = mutableListOf<Int>(),
+        private val intervalStrings: Map<Int, String> = mapOf(
         1 to "h",
         2 to "W",
         3 to "m3",
         4 to "M4"
     ),
-    private val allNotes: List<String> = listOf(
+        private val allNotes: List<String> = listOf(
         "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb",
         "G", "G#/Ab", "A", "A#/Bb", "B"
     ),
-    private val allMajorModes: List<String> = listOf(
+        private val allMajorModes: List<String> = listOf(
         "Major/Ionian", "Dorian", "Phrygian", "Lydian",
         "Mixolydian", "Minor/Aeolian", "Locrian"
     ),
-    private val pentaModes: List<String> = listOf(
+        private val pentaModes: List<String> = listOf(
         "Major Pentatonic", "Minor Pentatonic"
     ),
-    private val stepsMajorModes: List<Int> = listOf(2, 2, 1, 2, 2, 2, 1),
+        private val stepsMajorModes: List<Int> = listOf(2, 2, 1, 2, 2, 2, 1),
+        private val chordsMajorModes: List<String> = listOf(
+            "Major", "Minor", "Minor", "Major",
+            "Major", "Minor", "Diminished"
+    ),
     // val stepsNaturalMinor
     // val stepsMelodicMinor
     // val stepsByzantine
@@ -33,14 +40,15 @@ class Scale(
     }
 
     //
-    fun build(){
+    private fun build(){
         diatonicNotes.clear()   //
         cleanNotes.clear()      //
 
         diatonicNotes.add(rootNote) // add the root note to scale
         cleanNotes.add(rootNote)
 
-        setFormula(modeType)                // set formulaStrings and formulaInts to correct mode intervals
+        setFormula(modeType)        // set formulaStrings and formulaInts to correct mode intervals
+        setDiatonicChords()         //
 
         // LOOP FOR BUILDING SCALE
         var offset: Int = allNotes.indexOf(rootNote)
@@ -81,6 +89,10 @@ class Scale(
             print("${formulaStrings[i]} ")
         }
         println()
+        for (i in 0 until diatonicChords.size){
+            print("${diatonicChords[i]} ")
+        }
+        println()
     }
 
     fun getRoot() = rootNote
@@ -95,6 +107,8 @@ class Scale(
 
     fun getDiatonicNotes() = diatonicNotes
 
+    fun getDiatonicChords() = diatonicChords
+
     fun getCleanNotes() = cleanNotes
 
     fun setRoot(rootNote: String){ this.rootNote = rootNote; build() }
@@ -107,7 +121,7 @@ class Scale(
 
         if (modeType in allMajorModes) {
             var offset: Int = allMajorModes.indexOf(modeType)
-            for (i in 0 until stepsMajorModes.size){
+            for (i in stepsMajorModes.indices){
                 formulaInts.add(stepsMajorModes[offset])
                 formulaStrings.add(intervalStrings[formulaInts[i]]!!)
                 offset += 1
@@ -145,8 +159,28 @@ class Scale(
             println("Mode not found!!!")
         }
     }
+
+    private fun setDiatonicChords(){
+        diatonicChords.clear()  //
+
+        if(modeType == "Major/Ionian")
+            diatonicChords = mutableListOf("I", "ii", "iii", "IV", "V", "vi", "vii-")
+        else if (modeType == "Dorian")
+            diatonicChords = mutableListOf("i", "ii", "III", "IV", "v", "vi-", "VII")
+        else if (modeType == "Phrygian")
+            diatonicChords = mutableListOf("i", "II", "III", "iv", "v-", "VI", "vii")
+        else if (modeType == "Lydian")
+            diatonicChords = mutableListOf("I", "II", "iii", "iv-", "V", "vi", "vii")
+        else if (modeType == "Mixolydian")
+            diatonicChords = mutableListOf("I", "ii", "iii-", "IV", "v", "vi", "VII")
+        else if (modeType == "Minor/Aeolian")
+            diatonicChords = mutableListOf("i", "ii-", "III", "iv", "v", "VI", "VII")
+        else if (modeType == "Locrian")
+            diatonicChords = mutableListOf("i-", "II", "iii", "iv", "V", "VI", "vii")
+
+    }
 }
 
 fun main(){
-    //val scale = Scale(rootNote = "A", modeType = "Minor Blues")
+    val scale = Scale(rootNote = "C", modeType = "Major/Ionian")
 }
